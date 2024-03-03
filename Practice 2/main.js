7ccaa292d5d3724c93bf34d9ebc7ede1cb50b92f
welcome <jawadchaudhary00@gmail.com>
2024-03-03 15:37:52 +0500

    First Commit

---
+++ Practice 2/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//String Concatenation
+let firstname = "Jawad";
+let lastname = "Chaudhary";
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+//Template Literals
+let firstname1 = 'Obaid';
+let lastname1 = 'Chaudhary';
+let fullname1 = `${firstname1} ${lastname1}`; //`` are backticks.
+console.log(fullname1);
---
+++ Practice 2/main.ts
@@ -0,0 +1,13 @@
+//String Concatenation
+let firstname:string="Jawad";
+let lastname:string="Chaudhary";
+let fullname:string=firstname+" "+lastname;
+ console.log(fullname);
+
+ //Template Literals
+ let firstname1:string= 'Obaid';
+ let lastname1:string= 'Chaudhary';
+ let fullname1:string = `${firstname1} ${lastname1}`; //`` are backticks.
+ console.log(fullname1);
+
+ 
